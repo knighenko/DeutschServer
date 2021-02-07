a0e6278d309eacb9221588c84d9f5b8dc353cba4
Maksym Knyzhenko <knighenko@gmail.com>
2021-02-07 22:22:33 +0200

    Implements PostgreDB.java

--- src/jdbc/PostgreDB.java
+++ src/jdbc/PostgreDB.java
@@ -1,4 +1,30 @@
 package jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class PostgreDB {
+    //  Database credentials
+    static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/mydb";
+    static final String USER = "myuser";
+    static final String PASS = "123";
+    /**
+     * Create DB connection
+     */
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager
+                    .getConnection(DB_URL, USER, PASS);
+            // log.log(Level.INFO, "Connected to Database: " + DB_URL);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return connection;
+    }
 }
