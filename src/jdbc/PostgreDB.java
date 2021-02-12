443b129536c61dc613f298e93c97b3577d3a9c93
Maksym Knyzhenko <knighenko@gmail.com>
2021-02-12 23:55:30 +0200

    Implements PostgreDB.java

--- src/jdbc/PostgreDB.java
+++ src/jdbc/PostgreDB.java
@@ -83,24 +83,11 @@ public class PostgreDB {
         return flag;
     }
     /**
-     * Ьуе
+     * method return Progress of the user
      * */
-    public static boolean my() {
+    public static boolean myProgress(String e_mail) {
         boolean flag = false;
-        try {
-            Connection connection = getConnection();
-            PreparedStatement statement = connection.prepareStatement("insert into ACCOUNTS (e_mail,password,devicetoken,push) values (?,?,?,?)");
-            statement.setString(1, e_mail);
-            statement.setString(2, password);
-            statement.setString(3, deviceToken);
-            statement.setBoolean(4, false);
-            statement.executeUpdate();
-            statement.close();
-            closeConnection(connection);
-            flag = true;
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+
 
 
         return flag;
